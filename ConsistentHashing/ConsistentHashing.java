ee04d5371e57b02818f7128706b4b90b66efd03b
Rupesh <srupesh51@gmail.com>
2018-05-08 15:38:17 +0530

    Modified Consistent Algorithm

--- ConsistentHashing/ConsistentHashing.java
+++ ConsistentHashing/ConsistentHashing.java
@@ -7,11 +7,15 @@ class Request {
 }
 class DLL {
   LinkedList<Request> r;
+  int copyID;
   DLL prev;
   DLL next;
+  boolean failOver;
   DLL(){}
-  DLL(LinkedList<Request> t){
+  DLL(LinkedList<Request> t, int cID){
     this.r = t;
+    this.copyID = cID;
+    this.failOver = false;
     this.prev = null;
     this.next = null;
   }
@@ -66,39 +70,36 @@ class LoadBalancer {
   }
   public void addMachine(){
     int k = n/m;
-    int ID = 1;
+    int ID = 2;
     int num = k;
     int num1 = m;
     int num2 = -1;
+    int count = 0;
     for(int i = 0; i < n; i++){
       LinkedList<Request> r1 = null;
-      if(i == k) {
+      if(count < k || !map.containsKey(ID)){
+        r1 = !map.containsKey(ID) ?
+        new LinkedList<Request>() : map.get(ID);
+        r1.add(list.get(i));
+        map.put(ID,r1);
+        map.put(ID-1,r1);
+        count++;
+      }
+      if(count == k) {
         k = (n - (i+1));
         num1--;
+        if(num1 == 0){
+          break;
+        }
         num2 = (k)/(num1);
-        k = i + num2 + 1;
-        ID++;
-      }
-      if(i < k && !map.containsKey(ID)){
-        r1 = new LinkedList<Request>();
-        r1.add(list.get(i));
-        map.put(ID,r1);
-      } else if(i < k){
-        r1 = map.get(ID);
-        r1.add(list.get(i));
-        map.put(ID,r1);
-      }
-    }
-    for(int i = ID+1; i <= m*2; i++){
-      if(map.containsKey(ID)){
-        LinkedList<Request> r1 = map.get(ID);
-        map.put(i, r1);
-        ID--;
+        k = num2;
+        ID += 2;
+        count = 0;
       }
     }
   }
   public void print(){
-    for(int i = 1; i < map.size()+1; i++){
+    for(int i = 1; i <= map.size(); i++){
       LinkedList<Request> t = map.get(i);
       for(int j = 0; j < t.size(); j++){
         System.out.print(t.get(j).requestId+" ");
@@ -108,22 +109,67 @@ class LoadBalancer {
     System.out.println();
   }
   public void balanceLoads(){
-    for(int i = map.size(); i > 0; i--){
-      LinkedList<Request> t = map.get(i);
+    for(int i = 2; i <= map.size(); i += 2){
+      LinkedList<Request> t = !map.containsKey(i) ?
+      map.get(i-1) : map.get(i);
+      int cID = !map.containsKey(i) ? -1 : i-1;
       if(Machines == null){
-        Machines = new DLL(t);
+        Machines = new DLL(t,cID);
         Machines.prev = Machines;
         Machines.next = Machines;
       } else {
-        DLL m2 = new DLL(t);
+        DLL m2 = new DLL(t,cID);
         DLL t1 = Machines;
         DLL t2 = t1.prev;
-        m2.next = t1;
-        t1.prev = m2;
-        m2.prev = t2;
         t2.next = m2;
-        Machines = m2;
+        m2.prev = t2;
+        t1.prev = m2;
+        m2.next = t1;
       }
+    }
+  }
+  public void setFailOver(int n){
+    DLL m3 = Machines;
+    int ID = 2;
+    int count = 0;
+    for(int i = 2; i <= map.size(); i += 2){
+      if(i == ID){
+        LinkedList<Request> t = map.get(m3.copyID);
+        m3.r = t;
+        if(!m3.failOver){
+          m3.failOver = true;
+          map.remove(i);
+          m3.copyID = -1;
+        }
+        ID += 2;
+        count++;
+      }
+      if(count == n){
+        break;
+      }
+      m3 = m3.next;
+    }
+  }
+  public void rollBackFailOver(int n){
+    DLL m3 = Machines;
+    int ID = 2;
+    int count = 0;
+    for(int i = 1; i <= map.size(); i += 2){
+      if(i == ID-1){
+        LinkedList<Request> t = map.get(i);
+        m3.r = t;
+        if(m3.failOver){
+          m3.failOver = false;
+          map.put(ID, m3.r);
+          m3.copyID = i;
+        }
+        ID += 2;
+        count++;
+      }
+      if(count == n){
+        break;
+      }
+      m3 = m3.next;
     }
   }
   public void printLoadDistribution(){
@@ -131,21 +177,24 @@ class LoadBalancer {
     DLL m4 = m3.prev;
     DLL m5 = m3.prev.next;
     LinkedList<Request> t1 = m4.r;
+    System.out.print(m4.copyID+" ");
     for(int i = 0; i < t1.size(); i++){
       System.out.print(t1.get(i).requestId+" ");
     }
     LinkedList<Request> t2 = m5.r;
+    System.out.print(m5.copyID+" ");
     for(int i = 0; i < t2.size(); i++){
       System.out.print(t2.get(i).requestId+" ");
     }
     int count = 1;
     while(m3 != null){
       LinkedList<Request> t = m3.r;
+      System.out.print(m3.copyID+" ");
       for(int i = 0; i < t.size(); i++){
         System.out.print(t.get(i).requestId+" ");
       }
       System.out.println();
-      if(count == map.size()){
+      if(count == m){
         break;
       }
       count++;
@@ -156,15 +205,17 @@ class LoadBalancer {
 }
 public class ConsistentHashing {
   public static void main(String args[]){
-    LoadBalancer b1 = new LoadBalancer(6,3);
+    LoadBalancer b1 = new LoadBalancer(7,4);
     Request r[] = {new Request(1), new Request(3), new Request(2),
-    new Request(4), new Request(6), new Request(5)};
+    new Request(4), new Request(6), new Request(5), new Request(7)};
     for(int i = 0; i < r.length; i++){
       b1.addRequest(r[i]);
     }
     b1.addMachine();
     b1.print();
     b1.balanceLoads();
+    b1.setFailOver(2);
+    b1.rollBackFailOver(2);
     b1.printLoadDistribution();
   }
 }
